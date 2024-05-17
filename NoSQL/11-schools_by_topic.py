bcbc8740a60400c17ca19424e6f74268d3ccecaa
zabitaguliyeva <7644@holbertonstudents.com>
2024-05-17 19:44:44 +0400

    Where can I learn Python?

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 11. Where can I learn Python? """
+import pymongo
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ returns the list of school having a specific topic """
+    return mongo_collection.find({"topics": topic})
